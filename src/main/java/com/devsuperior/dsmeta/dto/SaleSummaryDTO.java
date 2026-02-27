d7ee355bec775456f4a13b54d869303adeec56c0
bgarbero <bruno_garbero@hotmail.com>
2026-02-27 20:18:07 -0300

    finalização

--- src/main/java/com/devsuperior/dsmeta/dto/SaleSummaryDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleSummaryDTO.java
@@ -4,45 +4,22 @@ import com.devsuperior.dsmeta.entities.Seller;
 
 public class SaleSummaryDTO {
 
-    private Long sellerId;
     private String sellerName;
-    private Double amount;
+    private Double total;
 
     public SaleSummaryDTO() {
     }
 
-    public SaleSummaryDTO(Long sellerId, String sellerName, Double amount) {
-        this.sellerId = sellerId;
+    public SaleSummaryDTO(String sellerName, Double total) {
         this.sellerName = sellerName;
-        this.amount = amount;
-    }
-
-    public SaleSummaryDTO(Seller seller) {
-        sellerId = seller.getId();
-        sellerName = seller.getName();
-    }
-
-    public Long getSellerId() {
-        return sellerId;
-    }
-
-    public void setSellerId(Long sellerId) {
-        this.sellerId = sellerId;
+        this.total = total;
     }
 
     public String getSellerName() {
         return sellerName;
     }
 
-    public void setSellerName(String sellerName) {
-        this.sellerName = sellerName;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
+    public Double getTotal() {
+        return total;
     }
 }
